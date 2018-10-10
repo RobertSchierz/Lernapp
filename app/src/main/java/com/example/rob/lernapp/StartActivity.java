d4e999b163d61f7d0984909bba3ac7d34d403f37
Robert Schierz <Robert.Schierz@hotmail.de>
2018-10-10 20:11:03 +0200

    Android Annotations funktioniert
    Gradleversion downgraden

--- app/src/main/java/com/example/rob/lernapp/StartActivity.java
+++ app/src/main/java/com/example/rob/lernapp/StartActivity.java
@@ -2,12 +2,24 @@ package com.example.rob.lernapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_start)
 public class StartActivity extends AppCompatActivity {
 
-    @Override
+
+    @Click(R.id.button)
+    void clickbutton(){
+        Toast.makeText(getApplicationContext(), "AMK", Toast.LENGTH_SHORT).show();
+    }
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    */
 }
