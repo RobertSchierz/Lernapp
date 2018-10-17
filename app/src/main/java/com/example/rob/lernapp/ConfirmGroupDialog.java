4300430a0228e90032e9c29d075a39693a787659
Robert Schierz <Robert.Schierz@hotmail.de>
2018-10-17 19:16:11 +0200

    Learngroup
    Actionbutton erstellt
    Dialog für Gruppen erstellen

--- app/src/main/java/com/example/rob/lernapp/ConfirmGroupDialog.java
+++ app/src/main/java/com/example/rob/lernapp/ConfirmGroupDialog.java
@@ -1,4 +1,72 @@
 package com.example.rob.lernapp;
 
-public class ConfirmGroupDialog {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+
+public class ConfirmGroupDialog extends DialogFragment {
+
+
+
+
+
+
+    public interface ConfirmGroupDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+    private Activity activity;
+    ConfirmGroupDialogListener cgdListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            cgdListener = (ConfirmGroupDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstancesState){
+        LayoutInflater inflater = this.activity.getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.activity);
+        builder.setMessage(R.string.permission_dialog)
+                .setView(inflater.inflate(R.layout.confirm_group_dialog_layout, null))
+                .setPositiveButton(R.string.confirm_group_dialog_positiv, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        cgdListener.onDialogPositiveClick(ConfirmGroupDialog.this);
+
+                    }
+                })
+                .setNegativeButton(R.string.confirm_group_dialog_negativ, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        cgdListener.onDialogNegativeClick(ConfirmGroupDialog.this);
+
+                    }
+                });
+
+        return builder.create();
+    }
+
+    void showdialog(Activity activity){
+        this.activity = activity;
+        onCreateDialog(new Bundle()).show();
+
+
+    }
+
 }
--- app/src/main/java/com/example/rob/lernapp/Learngroups.java
+++ app/src/main/java/com/example/rob/lernapp/Learngroups.java
@@ -1,13 +1,36 @@
 package com.example.rob.lernapp;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Toast;
 
-public class Learngroups extends AppCompatActivity {
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_learngroups)
+public class Learngroups extends AppCompatActivity implements ConfirmGroupDialog.ConfirmGroupDialogListener {
+
+
+
+    @AfterViews
+    void onCreate(){
+
+    }
+
+    @Click(R.id.learngroup_actionbutton)
+    void actionbutton_clicked(){
+        ConfirmGroupDialog confirmGroupDialog = new ConfirmGroupDialog();
+        confirmGroupDialog.showdialog(this);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learngroups);
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
     }
 }
